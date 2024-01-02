32ac1e1b31312b31b6fb3c9c60e17d146dc3cb2c
darley-raffael <darley.farias@segurosicoob.com.br>
2024-01-02 11:32:22 -0300

    test: unit test update user service

--- src/core/users/services/upadate.service.spec.ts
+++ src/core/users/services/upadate.service.spec.ts
@@ -1,5 +1,5 @@
-import { UserRepository } from "@/external/repositories/users/user.repository";
 import { describe, expect, it } from "bun:test";
+import { UserRepository } from "@/external/repositories/users/user.repository";
 import { RegisterUserService } from "./register.service";
 import { UpdateUserService } from "./update.service";
 
@@ -8,33 +8,76 @@ const sut = new UpdateUserService(userRepository); // sut = system under test
 const register = new RegisterUserService(userRepository);
 
 describe("UpdateUserService", () => {
-  it("should be able to update a user", async () => {
-    const newUser = {
-      name: "John Doe",
-      email: "john@example.com",
-      password: "123456",
-    };
+	it("should be able to update a user", async () => {
+		const newUser = {
+			name: "John Doe",
+			email: "john@example.com",
+			password: "123456",
+		};
 
-    const { user } = await register.execute({
-      name: newUser.name,
-      email: newUser.email,
-      password: newUser.password,
-    });
+		const { user } = await register.execute({
+			name: newUser.name,
+			email: newUser.email,
+			password: newUser.password,
+		});
 
-    const updatedUser = {
-      name: "Jane Doe",
-      email: "jane@example.com",
-      password: "123456",
-    };
+		const updatedUser = {
+			name: "Jane Doe",
+			email: "jane@example.com",
+			password: "123456",
+		};
 
-    const { user: updated } = await sut.execute({
-      id: user.id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-      password: updatedUser.password,
-      new_password: updatedUser.password,
-    });
+		const { user: updated } = await sut.execute({
+			id: user.id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			password: updatedUser.password,
+			new_password: updatedUser.password,
+		});
 
-    expect(updated.name).toBe(updatedUser.name);
-  });
+		expect(updated.name).toBe(updatedUser.name);
+	});
+
+	it("should not be able to update a user with an existing email", async () => {
+		const newUser = {
+			name: "John Doe",
+			email: "john@example.com",
+			password: "123456",
+		};
+
+		const { user } = await register.execute({
+			name: newUser.name,
+			email: newUser.email,
+			password: newUser.password,
+		});
+
+		const updatedUser = {
+			name: "Jane Doe",
+			email: "jane@example.com",
+			password: "123456",
+		};
+
+		await sut.execute({
+			id: user.id,
+			email: updatedUser.email,
+		});
+
+		expect(
+			sut.execute({ id: user.id, email: updatedUser.email }),
+		).rejects.toThrow();
+	});
+
+	it("should not be able to update a user without a new password", async () => {
+		const newUser = {
+			name: "John Doe",
+			email: "john@example.com",
+			password: "123456",
+		};
+
+		const { user } = await register.execute(newUser);
+
+		expect(
+			sut.execute({ id: user.id, password: newUser.password }),
+		).rejects.toThrow();
+	});
 });
--- src/core/users/services/update.service.ts
+++ src/core/users/services/update.service.ts
@@ -4,42 +4,44 @@ import type { UserRequestDTO } from "../dtos/register.dto";
 import type { UserRepositoryInterface } from "../repository/user-repository.interface";
 
 interface UserUpdateRequestDTO extends Partial<UserRequestDTO> {
-  id: string;
-  new_password?: string;
+	id: string;
+	new_password?: string;
 }
 
 interface UserUpdateResponseDTO {
-  user: User;
+	user: User;
 }
 
 export class UpdateUserService
-  implements UseCaseInterface<UserUpdateRequestDTO, UserUpdateResponseDTO>
+	implements UseCaseInterface<UserUpdateRequestDTO, UserUpdateResponseDTO>
 {
-  constructor(private userRepository: UserRepositoryInterface) {}
-  async execute(input: UserUpdateRequestDTO): Promise<UserUpdateResponseDTO> {
-    const { id, name, email, password, new_password } = input;
+	constructor(private userRepository: UserRepositoryInterface) {}
+	async execute(input: UserUpdateRequestDTO): Promise<UserUpdateResponseDTO> {
+		const { id, name, email, password, new_password } = input;
 
-    const user = await this.userRepository.findById(id);
-    if (!user) throw new Error("User not found");
+		const user = await this.userRepository.findById(id);
+		if (!user) throw new Error("User not found");
 
-    if (email) {
-      const findUserByEmail = await this.userRepository.findByEmail(email);
-      if (findUserByEmail) throw new Error("User already exists");
-    }
+		if (email) {
+			const findUserByEmail = await this.userRepository.findByEmail(email);
+			if (findUserByEmail) throw new Error("User already exists");
+		}
 
-    user.email = email ?? user.email;
-    user.name = name ?? user.name;
+		user.email = email ?? user.email;
+		user.name = name ?? user.name;
 
-    if (password && !new_password) throw new Error("New password is required");
+		if (password && !new_password) {
+			throw new Error("New password is required");
+		}
 
-    if (password && new_password) {
-      const passwordVerified = Bun.password.verifySync(password, user.password);
-      if (!passwordVerified) throw new Error("Password incorrect");
-      user.password = Bun.password.hashSync(new_password);
-    }
+		if (password && new_password) {
+			const passwordVerified = Bun.password.verifySync(password, user.password);
+			if (!passwordVerified) throw new Error("Password incorrect");
+			user.password = Bun.password.hashSync(new_password);
+		}
 
-    const userUpdated = await this.userRepository.update(user);
+		const userUpdated = await this.userRepository.update(user);
 
-    return { user: userUpdated };
-  }
+		return { user: userUpdated };
+	}
 }
