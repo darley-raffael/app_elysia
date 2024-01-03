55ed2c2969057662ba85355b43cdbd050c1afc5d
darley-raffael <darleyraffael@gmail.com>
2024-01-03 09:24:29 -0300

    chore: change configs of the tests

--- src/controllers/user-controller/register.controller.spec.ts
+++ src/controllers/user-controller/register.controller.spec.ts
@@ -1,26 +1,26 @@
-import { app } from "@/app";
 import { describe, expect, it } from "bun:test";
+import { app } from "@/app";
 
-const baseUrl = `http://localhost:3333/`;
+const baseUrl = "http://localhost:3333/";
 
 describe("Register Controller", async () => {
-  it("should be able to register a new user", async () => {
-    const newUser = {
-      name: "John Doe",
-      email: "qjwzA@example.com",
-      password: "123456",
-    };
+	it("should be able to register a new user", async () => {
+		const newUser = {
+			name: "John Doe",
+			email: "qjwzA@example.com",
+			password: "123456",
+		};
 
-    const response = await app.handle(
-      new Request(`${baseUrl}users`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newUser),
-      })
-    );
+		const response = await app.handle(
+			new Request(`${baseUrl}users`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newUser),
+			}),
+		);
 
-    expect(response.status).toBe(200);
-  });
+		expect(response.status).toBe(200);
+	});
 });
