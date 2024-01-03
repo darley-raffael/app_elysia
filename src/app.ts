1103ce5a5a14efeb729805265aa641336b9c22c5
Dárley Raffael <darleyraffael@gmail.com>
2024-01-02 22:51:45 -0300

    feat: register user controller

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
 import Elysia from "elysia";
+import { registerController } from "./controllers/user-controller/register.controller";
 
 export const app = new Elysia();
+
+app.use(registerController);
